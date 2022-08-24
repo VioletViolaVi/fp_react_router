64ef2fc3c6e3e153fb212c7c92ae28c346a9aff5
vivian <vivian.v18723@gmail.com>
2022-08-24 11:37:45 +0100

    import components add routes for components

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,32 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import Cohort from "./pages/Cohort"
+import Header from "./layouts/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCohort from "./pages/SingleCohort";
+import AllCohorts from "./pages/AllCohorts";
 
 function App() {
   return (
     /*
      - only <Route></Route> can go inside <Routes></Routes>
-     - must create component when placeholder is in path
+     - must create component when placeholder (/:fishFingers) is in path
     */
     <Routes>
+      <Route path="/" element={<Header />}>
+        
+        <Route path="cohorts" element={<AllCohorts />}>
+          <Route path=":fishFingers" element={<SingleCohort />}></Route>
+        </Route>
+
+        <Route path="about" element={<About />}></Route>
+
+        <Route index element={<Home />}></Route>
+        
+      </Route>
+
       <Route path="*" element={<h1>Not Found!</h1>}></Route>
-      <Route path="/" element={<h1>Hello, World!</h1>}></Route>
-      <Route path="cohorts/:fishFingers" element={<Cohort />}></Route>
     </Routes>
   );
 }
